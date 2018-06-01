fd357d0c4d6fcae23ba02903aac1f510a8b3541e
Andris Praulitis <andris@kwtdesign.co.uk>
2018-06-01 20:53:23 +0100

    Decode html entities (#25)
    * Decode html entities
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js

--- src/index.js
+++ src/index.js
@@ -22,8 +22,10 @@ var Script2 = {
         if (!this.src) {
           Script2.p = Script2.p.then(() => {
             var s = document.createElement('script')
+            var h = this.$el.innerHTML
+            h = h.replace(/&lt;/gi, '<').replace(/&gt;/gi, '>').replace(/&amp;/gi, '&')
             s.type = 'text/javascript'
-            s.appendChild(document.createTextNode(this.$el.innerHTML))
+            s.appendChild(document.createTextNode(h))
             parent.appendChild(s)
           })
         } else {
